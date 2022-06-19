ce912d85ba2b19a844d4bc098cc49b6c6539ab69
bafarrar <bafarrar@yahoo.com>
2022-06-19 17:46:34 -0500

    Create Create_All_functions.sql

---
+++ sql/functions/Create_All_functions.sql
@@ -0,0 +1,21 @@
+
+--
+-- Run from psql command
+--
+-- psql -d dcd -f Create_All_functions.sql
+--
+--   -d is the Database to process
+--
+--   Other parameters which may be needed to identify instance of postgres
+--     -h DB host name
+--     -U DB user
+--
+
+\i GetCities_Function.sql
+\i GetStreets_Function.sql
+\i Get_Addresses_Function.sql
+\i Get_Precincts_Function.sql
+\i Get_Registered_Voters.sql
+\i Get_Unregistered_Voters.sql
+\i Update_Address_Function.sql
+
